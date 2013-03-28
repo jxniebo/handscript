ce1db72f387ed8540134d54d725110caf5451fa4
jxniebo <jxniebo@yahoo.cn>
2013-03-28 15:27:59 +0800

    restructure

--- hand/c/exercise/message.h
+++
@@ -1,9 +0,0 @@
-#ifndef HEAD_MESSAGE_H
-#define HEAD_MESSAGE_H 1
-
-#define DIVIDE_LINE "\n-------------------------------------------------------\n"
-#define PROGRAM_FUNC_LABEL "This program is used for: "
-#define PROGRAM_FUNC_FIGURE1 "figure how many '1' exist in a binary value "
-#define INPUT_NOTE "need you to kindly input a value "
-
-#endif
