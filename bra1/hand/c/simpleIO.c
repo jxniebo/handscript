d718531b7cb71c6a106e313a2594367eb9a7ff54
jxniebo <jxniebo@yahoo.cn>
2013-03-28 15:19:50 +0800

    restructure

--- bra1/hand/c/simpleIO.c
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include "./algorithm/headers/sortAlgorithm.h"
-
-extern int externalTest;
-int main() {
-	std::cout << externalTest << std::endl;
-	return 0;
-}
--- bra1/hello.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	printf("hello world\n");// just add some comment to show change
-
-	return 0;
-}
